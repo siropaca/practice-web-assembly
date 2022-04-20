17bcc299cda0a664d1144cb094488e27771a2d60
siropaca <ryhshr3@gmail.com>
2022-04-20 23:43:54 +0900

    modify: AddInt

--- src/AddInt.js
+++ src/AddInt.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
-const bytes = fs.readFileSync(__dirname + '/AddInt.wasm');
-const value_1 = parseInt(process.argv[2]);
-const value_2 = parseInt(process.argv[3]);
-
 (async  () => {
+  const bytes = fs.readFileSync( `${__dirname}/AddInt.wasm`);
+
+  const value1 = parseInt(process.argv[2]);
+  const value2 = parseInt(process.argv[3]);
+
   const obj = await WebAssembly.instantiate(new Uint8Array(bytes));
-  let add_value = obj.instance.exports.AddInt(value_1, value_2);
-  console.log(`${value_1} + ${value_2} = ${add_value}`);
+  const addValue = obj.instance.exports.AddInt(value1, value2);
+
+  console.log(`${value1} + ${value2} = ${addValue}`);
 })()
